f63afcae6dc3034aee9308e29d60987f4fc6580d
Matteo Paolucci <matteopaolucci99@gmail.com>
2023-04-26 14:38:12 +0200

    Improved page structure and added personal description

--- js/main.js
+++ js/main.js
@@ -2,22 +2,12 @@
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
 var keys = {33: 1, 38: 1, 34: 1, 40: 1};
 
-var supportsPassive = true;
-try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
-var old_scroll_top = 0;
-
 $(document).ready(function () {
-    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+    $("main > div").get(0).scrollIntoView({behavior: 'smooth', block: 'center'});
+    $(document).keydown(preventDefaultForScrollKeys);
     $("li#github > ul").click(() => {
         let arrow = "li#github > ul > li:last-child > i";
-        $("li#github > section").slideToggle();
+        $("li#github > div").slideToggle();
         $(arrow).fadeOut(() => {
             if($(arrow).hasClass("fa-chevron-down")) {
                 $(arrow).removeClass("fa-chevron-down");
@@ -29,20 +19,7 @@ $(document).ready(function () {
             $(arrow).fadeIn();
         });
     });
-    $(document).scroll(newScroll);
-    window.addEventListener("touchmove", newScroll);
 });
-
-function newScroll() {
-    var current_scroll_top = $(document).scrollTop();
-    var scroll_delta = current_scroll_top - old_scroll_top;
-    old_scroll_top = current_scroll_top;
-    if (scroll_delta > 0) {
-        $("#home").get(0).scrollIntoView({behavior: 'smooth'});
-    } else {
-        $('body > div:first-child').get(0).scrollIntoView({behavior: 'smooth'});
-    }
-}
 
 function preventDefaultForScrollKeys(e) {
     if (keys[e.keyCode]) {
@@ -50,7 +27,7 @@ function preventDefaultForScrollKeys(e) {
         if(e.keyCode === 40 || e.keyCode === 34) {
             $("#home").get(0).scrollIntoView({behavior: 'smooth'});
         } else if (e.keyCode === 38 || e.keyCode === 33) {
-            $('body > div:first-child').get(0).scrollIntoView({behavior: 'smooth'});
+            $('main').get(0).scrollIntoView({behavior: 'smooth'});
         }
       return false;
     }
